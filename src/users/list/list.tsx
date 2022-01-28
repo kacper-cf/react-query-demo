d4d40121f213fabc04ff9bcd265e0b7f321d9100
kacper-cf <kacper@couponfollow.com>
2022-01-28 14:41:22 +0100

    Fix stale requests

--- src/users/list/list.tsx
+++ src/users/list/list.tsx
@@ -3,6 +3,7 @@ import {
   CircularProgress,
   Flex,
   Table,
+  Tbody,
   Td,
   Th,
   Thead,
@@ -13,7 +14,7 @@ import toast from "react-hot-toast";
 import { useMutation, useQuery } from "react-query";
 import { fetchUsers, deleteUser } from "../store";
 export const UserList: FC = () => {
-  const { isLoading, data, error, refetch } = useQuery("users", fetchUsers);
+  const { isFetching, data, error, refetch } = useQuery("users", fetchUsers);
 
   const removeUserMutation = useMutation(
     "deleteUser",
@@ -26,7 +27,7 @@ export const UserList: FC = () => {
     }
   );
 
-  if (isLoading) {
+  if (isFetching) {
     return (
       <Flex justifyContent={"center"} alignItems={"center"}>
         <CircularProgress isIndeterminate />
@@ -50,21 +51,23 @@ export const UserList: FC = () => {
           <Th>Name and surname</Th>
         </Tr>
       </Thead>
-      {data?.map(({ fullName, id }) => (
-        <Tr>
-          <Td>{id}</Td>
-          <Td>{fullName}</Td>
-          <Td>
-            <Button
-              colorScheme={"red"}
-              onClick={() => removeUserMutation.mutate(id)}
-              isLoading={removeUserMutation.status === "loading"}
-            >
-              Remove a user
-            </Button>
-          </Td>
-        </Tr>
-      ))}
+      <Tbody>
+        {data?.map(({ fullName, id }) => (
+          <Tr>
+            <Td>{id}</Td>
+            <Td>{fullName}</Td>
+            <Td>
+              <Button
+                colorScheme={"red"}
+                onClick={() => removeUserMutation.mutate(id)}
+                isLoading={removeUserMutation.variables === id}
+              >
+                Remove a user
+              </Button>
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
     </Table>
   );
 };
