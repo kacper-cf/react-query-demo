7393a578ffce9b458326b696b70b8265e6a0d556
kacper-cf <kacper@couponfollow.com>
2022-01-31 10:47:56 +0100

    Add load more demo

---
+++ src/passengers/loadMore/loadMore.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import { FC } from "react";
+import { InfiniteData, useInfiniteQuery } from "react-query";
+import { fetchPassengers, PassengersCollectionDto } from "../store/passengers";
+
+const mapPagesToComponents = (
+  infiniteData: InfiniteData<PassengersCollectionDto> | undefined
+) => {
+  if (!infiniteData) {
+    return [];
+  }
+
+  return infiniteData.pages.map(({ data }, pageIndex) => {
+    return data.map(({ name, trips }, index) => (
+      <Tr key={`${name}_${trips}_${index}`}>
+        <Td textAlign={"center"}>{pageIndex}</Td>
+        <Td textAlign={"center"}>{name}</Td>
+        <Td textAlign={"center"}>{trips || 0}</Td>
+      </Tr>
+    ));
+  });
+};
+
+export const LoadMorePassengers: FC = () => {
+  const { fetchNextPage, data, isFetching } = useInfiniteQuery(
+    "passengers",
+    ({ pageParam }) => fetchPassengers(pageParam),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.totalPages !== pages.length;
+      },
+    }
+  );
+
+  return (
+    <div>
+      <Table variant={"simple"} width={"50rem"}>
+        <Thead>
+          <Tr>
+            <Th>From page</Th>
+            <Th>Name</Th>
+            <Th>Trips</Th>
+          </Tr>
+        </Thead>
+        <Tbody>{mapPagesToComponents(data)}</Tbody>
+      </Table>
+      <Box
+        width="50rem"
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-around"}
+        my={15}
+      >
+        <Button isLoading={isFetching} onClick={() => fetchNextPage()}>
+          Load more
+        </Button>
+      </Box>
+    </div>
+  );
+};
