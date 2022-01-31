22024e4a4bba2966c962515539398b017109210d
kacper-cf <kacper@couponfollow.com>
2022-01-31 12:01:18 +0100

    Add query invalidation

--- src/welcome/welcome.tsx
+++ src/welcome/welcome.tsx
@@ -1,10 +1,35 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, SimpleGrid } from "@chakra-ui/react";
 import { FC } from "react";
+import { queryClient } from "../query/queryProvider";
+
+const invalidateQuery = (queryName?: string) => {
+  if (queryName) {
+    return queryClient.invalidateQueries(queryName);
+  }
+
+  queryClient.invalidateQueries();
+};
 
 export const Welcome: FC = () => {
+  const invalidateAllQueries = () => invalidateQuery();
+  const invalidateUsers = () => queryClient.invalidateQueries("users");
+
   return (
-    <div>
+    <Box marginRight={"15"}>
       <Heading size="md">Welcome in react query demo!</Heading>
-    </div>
+      <SimpleGrid>
+        <Button
+          colorScheme={"red"}
+          mt={15}
+          mb={15}
+          onClick={invalidateAllQueries}
+        >
+          Invalidate all queries!
+        </Button>
+        <Button colorScheme={"red"} mt={15} mb={15} onClick={invalidateUsers}>
+          Invalidate users query!
+        </Button>
+      </SimpleGrid>
+    </Box>
   );
 };
